f99b9fb4c30c2f2a6dbc71c4357cd03d3a23942f
johnmccants002 <johnmccants002@gmail.com>
2022-08-20 19:03:18 -0700

    Initial Commit

---
+++ MinimalContactRestaurant/MinimalContactRestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalContactRestaurantApp.swift
+//  MinimalContactRestaurant
+//
+//  Created by John McCants on 8/20/22.
+//
+
+import SwiftUI
+
+@main
+struct MinimalContactRestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
